009fafb025ab692df015da1499cf44ff4f49bc60
devmicro <bhattrahul270@gmail.com>
2023-04-10 01:09:29 +0530

    adding basic interfaces for later use

---
+++ src/common/interface/index.ts
@@ -0,0 +1,15 @@
+import {
+    RequestDispatcher,
+    GetRequest,
+    PostRequest,
+    PutRequest,
+    DeleteRequest,
+} from './RequestDispatcher';
+
+export type {
+    RequestDispatcher,
+    GetRequest,
+    PostRequest,
+    PutRequest,
+    DeleteRequest,
+};
